3d5be26675860d49a463e3a6cfc4dfca4532ada0
jimin.yoon <ccc625@naver.com>
2017-03-25 14:21:33 +0900

    메인 엑티비티 기능 구현 팀선택 엑티비티 비쥬얼 처리 수정

---
+++ KGY_Product/app/src/main/java/com/example/kgy_product/TeamMakeActivity.java
@@ -0,0 +1,18 @@
+package com.example.kgy_product;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ccc62 on 2017-03-25.
+ */
+
+public class TeamMakeActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_team);
+    }
+}
