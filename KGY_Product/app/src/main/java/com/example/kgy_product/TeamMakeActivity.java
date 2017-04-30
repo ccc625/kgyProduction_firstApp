319d2a27ce7dde115ea3eb53b7e01825f46f3f0a
lee-PC\lee <woojin126@naver.com>
2017-04-30 12:45:42 +0900

    uses-permission 3줄 퍼미션위한 메니페스트추가. name  registeActivity로 바꿈

--- KGY_Product/app/src/main/java/com/example/kgy_product/TeamMakeActivity.java
+++ KGY_Product/app/src/main/java/com/example/kgy_product/TeamMakeActivity.java
@@ -2,6 +2,8 @@ package com.example.kgy_product;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 /**
  * Created by ccc62 on 2017-03-25.
@@ -9,10 +11,28 @@ import android.support.v7.app.AppCompatActivity;
 
 public class TeamMakeActivity extends AppCompatActivity
 {
-    @Override
+    private RadioButton btnMan;
+    private RadioButton btnWoman;
+    private Button btnPrev;
+    private Button btnNext;
+    String[] items={"1명","2명","3명","4명","5명 ","6명"};
+
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_team);
+
+        init();
+    }
+
+    private void init()
+    {
+        initDisplayObject();
+
+    }
+    private void initDisplayObject()
+    {
+        btnPrev = (Button) findViewById(R.id.btnPrev);
+        btnNext = (Button) findViewById(R.id.btnNext);
     }
 }
