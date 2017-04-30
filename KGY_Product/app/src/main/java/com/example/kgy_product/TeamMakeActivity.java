7a23511fbd364046701dde01f2a0e22b98f62fcf
jimin.yoon <ccc625@naver.com>
2017-04-30 13:20:21 +0900

    팀만들기 액티비티 클래스 수정

--- KGY_Product/app/src/main/java/com/example/kgy_product/TeamMakeActivity.java
+++ KGY_Product/app/src/main/java/com/example/kgy_product/TeamMakeActivity.java
@@ -2,6 +2,11 @@ package com.example.kgy_product;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 /**
  * Created by ccc62 on 2017-03-25.
@@ -9,10 +14,73 @@ import android.support.v7.app.AppCompatActivity;
 
 public class TeamMakeActivity extends AppCompatActivity
 {
+    private Button btnMan;
+    private Button btnWoman;
+
+    private EditText txtTeamName;
+    private Spinner memberCountSpinner;
+
+    private BottomLayout bottomLayout;
+
+    private View.OnClickListener buttonClickListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_team);
+
+        init();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        btnMan = null;
+        btnWoman = null;
+
+        txtTeamName = null;
+        memberCountSpinner = null;
+
+        bottomLayout = null;
+
+        super.onDestroy();
+    }
+
+    private void init()
+    {
+        initDisplayObject();
+        initListener();
+    }
+
+    private void initDisplayObject()
+    {
+        btnMan = (Button) findViewById(R.id.btnMan);
+        btnWoman = (Button) findViewById(R.id.btnWoman);
+
+        txtTeamName = (EditText) findViewById(R.id.txtTeamName);
+        memberCountSpinner = (Spinner) findViewById(R.id.memberCountSpinner);
+
+        bottomLayout = new BottomLayout(this);
+        addContentView( bottomLayout, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT) );
+    }
+
+    private void initListener()
+    {
+        buttonClickListener = new View.OnClickListener()
+        {
+            @Override
+            public void onClick( View view )
+            {
+                if( view.getId() == btnMan.getId() )
+                {
+
+                }
+                else if( view.getId() == btnWoman.getId() )
+                {
+
+                }
+            }
+        };
     }
 }
