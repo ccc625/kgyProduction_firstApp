613107ae0e2318e7c0616d797c2dad20e3355034
deokhwan <dhdh9224@naver.com>
2017-10-29 15:28:11 +0900

    로그인 세션 구현

--- KGY_Product/app/src/main/java/com/example/kgy_product/MainActivity.java
+++ KGY_Product/app/src/main/java/com/example/kgy_product/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.kgy_product;
 
 import android.content.Intent;
+
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,9 @@ import com.example.kgy_product.networkTask.NetworkTask;
 
 import org.json.JSONObject;
 import org.json.JSONStringer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -31,19 +36,27 @@ public class MainActivity extends AppCompatActivity
 
     private void init()
     {
-        initDisplayObject();
-        initListener();
+        //TODO 테스트용 삭제 예정
+        saveLogin();
 
-        AmfAdaptor.AmfCallback callback = new AmfAdaptor.AmfCallback() {
-            @Override
-            public void onResponse(JSONObject data)
-            {
-                System.out.println( data );
-            }
-        };
+        if(isLoginCheck()){
+            //TODO 디비 접속 로그인
+        } else {
+            initDisplayObject();
+            initListener();
+
+            AmfAdaptor.AmfCallback callback = new AmfAdaptor.AmfCallback() {
+                @Override
+                public void onResponse(JSONObject data)
+                {
+                    System.out.println( data );
+                }
+            };
 
 //        AmfAdaptor.instance().initUser( callback, "tokenToKen");
 //        AmfAdaptor.instance().openSampleList4( callback, "yjm", "20", "1");
+        }
+
     }
 
     private void initDisplayObject()
@@ -85,5 +98,75 @@ public class MainActivity extends AppCompatActivity
         Intent intent = new Intent(getApplicationContext(),TeamMakeActivity.class);
         intent.putExtra("location",location);
         startActivity(intent);
+    }
+
+    private boolean isLoginCheck(){
+        SharedPreferences setting = getSharedPreferences("setting",MODE_PRIVATE);
+        String loginDate = setting.getString("date","");
+
+        String now = nowDate();
+
+        if(!(loginDate != null && !"".equals(loginDate)) ){
+            return false;
+        }
+
+        if(Integer.parseInt(loginDate) == Integer.parseInt(now)){
+            return true;
+        } else {
+            SharedPreferences.Editor editor = setting.edit();
+            editor.remove("date");
+            editor.remove("id");
+            editor.commit();
+            return false;
+        }
+    }
+
+    private String nowDate(){
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+
+        SimpleDateFormat sdf =  new SimpleDateFormat("yyyy/MM/dd/HH/mm");
+        String nowDate = sdf.format(date);
+
+        String result = "";
+
+        if(Integer.parseInt(nowDate.split("/")[3]) <= 6){
+            for(int i = 0; i < nowDate.split("/").length; i++){
+                if(i == 1){
+                    int mm = Integer.parseInt(nowDate.split("/")[i]) - 1;
+                    result += String.valueOf(mm);
+                } else if(i > 2){
+                    break;
+                }
+                else {
+                    result += nowDate.split("/")[i];
+                }
+            }
+        } else {
+            for(int i = 0; i < nowDate.split("/").length; i++){
+                if(i > 2){
+                    break;
+                } else {
+                    result += nowDate.split("/")[i];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    //TODO 테스트용 삭제 예정
+    private void saveLogin(){
+        SharedPreferences setting = getSharedPreferences("setting",MODE_PRIVATE);
+        SharedPreferences.Editor editor = setting.edit();
+
+        Date date = new Date();
+
+        SimpleDateFormat sdf =  new SimpleDateFormat("yyyyMMdd");
+        String nowDate = sdf.format(date);
+
+        editor.putString("date",nowDate);
+        editor.putString("id","sss");
+        editor.commit();
     }
 }
